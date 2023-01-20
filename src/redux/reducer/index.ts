18719377ece5dabe7938a7784a64275c0fa68458
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-01-20 12:13:15 -0800

    orders, filters and categories

--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -1,12 +1,16 @@
 import { AnyAction } from "redux";
-import { GET_PRODUCTS } from "../actions/Products";
+import { FILTER_BY_CATEGORY, FILTER_BY_NAME, GET_CATEGORIES, GET_PRODUCTS, ORDER_BY_NAME } from "../actions/Products";
 
 interface StateProducts {
   products: any[]
+  productsBackUp: any[]
+  categories: any[]
 }
 
 const initialState: StateProducts = {
-  products: []
+  products: [],
+  productsBackUp: [],
+  categories: []
 };
 
 function rootReducer(state: StateProducts = initialState, action: AnyAction) {
@@ -14,8 +18,47 @@ function rootReducer(state: StateProducts = initialState, action: AnyAction) {
     case GET_PRODUCTS:
       return {
         ...state,
+        products: action.payload,
+        productsBackUp: action.payload
+      }
+    case GET_CATEGORIES:
+      return {
+        ...state,
+        categories: action.payload
+      }
+    case FILTER_BY_NAME:
+      return {
+        ...state,
         products: action.payload
       }
+    case FILTER_BY_CATEGORY:
+      let filteredCategory;
+      if(action.payload === 'electronics'){
+        filteredCategory = state.productsBackUp.filter((elem) => elem.category === action.payload)
+      } else if (action.payload === 'jewelery'){
+        filteredCategory = state.productsBackUp.filter((elem) => elem.category === action.payload)
+      } else if (action.payload === "men's clothing"){
+        filteredCategory = state.productsBackUp.filter((elem) => elem.category === action.payload)
+      } else if (action.payload === "women's clothing"){
+        filteredCategory = state.productsBackUp.filter((elem) => elem.category === action.payload)
+      }
+      return {
+        ...state,
+        products: action.payload === 'all' ? state.productsBackUp : filteredCategory
+      }
+    case ORDER_BY_NAME:
+      const products = state.products;
+      let orderedName;
+      if(action.payload === 'asc'){
+        orderedName = products.sort((a,b) => a.title.localCompare(b.title))
+      } else if(action.payload === 'desc'){
+        orderedName = products.sort((a,b) => b.title.localCompare(a.title))
+      }
+      return{
+        ...state,
+        products: orderedName
+      }
+    
     default:
       return state;
   }
