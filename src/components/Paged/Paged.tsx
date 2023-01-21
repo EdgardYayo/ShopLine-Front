8e11bf16d5b85db8f0dbe060f41e6075ff310f11
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-01-21 11:09:02 -0800

    paged and styles

---
+++ src/components/Paged/Paged.tsx
@@ -0,0 +1,31 @@
+import React, { MouseEventHandler } from "react";
+import finalPropsSelectorFactory from "react-redux/es/connect/selectorFactory";
+import style from "./Paged.module.css";
+
+interface pagedElements {
+    paged: Function
+    productsPerPage: number
+    allProducts: any
+}
+
+
+
+export default function Paged({ allProducts, productsPerPage, paged }:pagedElements): JSX.Element {
+    let numbers = []
+    for(let i = 1; i<= Math.ceil(allProducts/productsPerPage); i++){
+        numbers.push(i)
+    }
+
+
+    return (
+        <div>
+        {
+            numbers.length && numbers.map(n => {
+                return(
+                    <button className={style["paged-button"]} key={n} onClick={() => paged(n)}>{n}</button>
+                    )
+                })
+            }
+        </div>
+    )
+}
