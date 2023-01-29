b55bede671732c2ec0f418f3295c47bf6a038080
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-01-28 16:34:37 -0800

    Better styles for reviews

--- src/components/ShowReviews/ShowReviews.tsx
+++ src/components/ShowReviews/ShowReviews.tsx
@@ -4,30 +4,29 @@ import { useAppDispatch, useAppSelector } from "../../redux/store/hooks";
 import style from "../../style/ShowReviews/ShowReviews.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-import Loading from "../../utils/Loading";
 
+export default function ShowReviews(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const allReviews = useAppSelector((state) => state.reviews);
 
+  useEffect(() => {
+    dispatch(getReviews());
+  }, [dispatch]);
 
-export default function ShowReviews():JSX.Element {
-
-    const dispatch = useAppDispatch()
-    const allReviews = useAppSelector(state => state.reviews)
-
-    useEffect(() => {
-        dispatch(getReviews())
-    },[dispatch])
-
-
-    return (
-        <div className={style["container"]}>
-            {allReviews && allReviews.map(rev => {
-                return (
-                    <div className={style["sub-container"]} key={rev.id}>
-                        <p className={style["content"]}>{rev.content}</p>
-                        <p className={style["rating"]}><FontAwesomeIcon className={style["icon"]} icon={faStar}/> {rev.rating}</p>
-                    </div>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className={style["container"]}>
+      {allReviews &&
+        allReviews.map((rev) => {
+          return (
+            <div className={style["sub-container"]} key={rev.id}>
+              <p className={style["content"]}>{rev.content}</p>
+              <p className={style["rating"]}>
+                <FontAwesomeIcon className={style["icon"]} icon={faStar} />
+                {rev.rating}
+              </p>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
