8f2934257d484cd6224a9e8edf39ca515d56d5e7
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-02-01 15:01:06 -0800

    fix cart details, cleaning user info

--- src/components/NavBar/Logout-button.tsx
+++ src/components/NavBar/Logout-button.tsx
@@ -1,19 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useAppDispatch } from "../../redux/store/hooks";
-import { cleanUserInfo } from "../../redux/actions/Users";
 
 const LogoutButton: React.FC = () => {
   const { logout } = useAuth0();
-  const dispatch = useAppDispatch()
 
   const handleLogout = () => {
-    dispatch(cleanUserInfo())
     logout({
       returnTo: window.location.origin,
     });
 
-    
   };
 
   return (
